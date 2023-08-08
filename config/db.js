f5ad158e7aa112654bcd54d63f44e8340cfdb5c6
karib-arian <karibarian12@gmail.com>
2023-08-08 22:20:07 +0600

    db updated

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 
 require("dotenv").config();
 
-const { Client, Pool } = require("pg");
-const pool = new Client({
-    host : "localhost",
-    user:"postgres",
-    port:5432,
-    password:"1234",
-    database:process.env.DATABASE
+// const { Client, Pool } = require("pg");
+// const pool = new Client({
+//     host : "localhost",
+//     user:"postgres",
+//     port:5432,
+//     password:"1234",
+//     database:process.env.DATABASE
 
-});
+// });
 
-// const pool = new Pool({
-//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-//   })
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
 
 pool.connect()
 module.exports = pool;
