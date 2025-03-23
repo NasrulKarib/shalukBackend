c264c17573b44c7c2a8e855e77013311e9312355
NasrulKarib <karibarian12@gmail.com>
2025-03-23 15:51:16 +0600

    modify db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,9 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
-require("dotenv").config();
-
-const { Client } = require("pg");
-// const pool = new Client({
-//     host : "localhost",
-//     user:"postgres",
-//     port:5432,
-//     password:"1234",
-//     database:process.env.DATABASE
-
-// });
-
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-  })
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } // Required for NeonDB
+});
 
-pool.connect()
 module.exports = pool;
